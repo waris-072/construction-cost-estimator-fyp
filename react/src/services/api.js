0957a007bb9d4c38262a19f2dde6493f61efd433
waris-072 <warishakrow3@gmail.com>
2026-06-13 16:39:48 +0500

    prepare frontend for netlify deployment

--- react/src/services/api.js
+++ react/src/services/api.js
@@ -1,7 +1,9 @@
 // src/services/api.js - COMPLETE UPDATED VERSION
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL 
+  ? `${process.env.REACT_APP_API_URL}/api` 
+  : 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
